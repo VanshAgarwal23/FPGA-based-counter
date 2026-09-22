cb2c06fda2c03aae63cb029597fde876d8972139
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-22 15:02:30 +0530

    created two new modules 'bin_to_bcd' & 'display_mux'

---
+++ FPGA-Based_Counter_System.srcs/sources_1/new/bin_to_bcd.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.09.2026 15:00:25
+// Design Name: 
+// Module Name: bin_to_bcd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bin_to_bcd(
+
+    );
+endmodule
---
+++ FPGA-Based_Counter_System.srcs/sources_1/new/display_mux.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.09.2026 15:00:53
+// Design Name: 
+// Module Name: display_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module display_mux(
+
+    );
+endmodule
