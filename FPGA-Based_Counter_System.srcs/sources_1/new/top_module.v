cef0cb9f6d7da640d28224ac7134e81f798ca7f8
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-22 15:22:45 +0530

    designed RTL code for all remaining modules

--- FPGA-Based_Counter_System.srcs/sources_1/new/top_module.v
+++ FPGA-Based_Counter_System.srcs/sources_1/new/top_module.v
@@ -1,26 +1,34 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.09.2026 14:54:24
-// Design Name: 
-// Module Name: top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module top_module #(parameter MUX_SPEED = 2) ( 
+    input clk,
+    input rst,
+    input en,
+    input dir,
+    output [6:0] seg,  
+    output [3:0] an    
+);
+    wire [3:0] count_wire;
+    wire [3:0] bcd_tens;
+    wire [3:0] bcd_ones;
+    wire [3:0] current_digit;
 
+    // 1. The Counter
+    up_down_counter #(.N(4)) my_counter (
+        .clk(clk), .rst(rst), .en(en), .dir(dir), .q(count_wire)
+    );
 
-module top_module(
+    // 2. Binary to BCD Converter
+    bin_to_bcd b2bcd (
+        .bin(count_wire), .tens(bcd_tens), .ones(bcd_ones)
+    );
 
+    // 3. Display Multiplexer
+    display_mux #(.DIVISOR(MUX_SPEED)) mux (
+        .clk(clk), .bcd_tens(bcd_tens), .bcd_ones(bcd_ones),
+        .current_digit(current_digit), .an(an)
+    );
+
+    // 4. 7-Segment Decoder
+    hex_to_7seg decoder (
+        .bin_in(current_digit), .seg_out(seg)
     );
 endmodule
