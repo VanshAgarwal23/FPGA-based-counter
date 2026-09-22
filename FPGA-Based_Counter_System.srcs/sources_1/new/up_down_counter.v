67e0ab164c1dc111dc8b74046a91f01aa2a49be8
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-22 15:08:29 +0530

    Designed RTL for up_down_counter Module

--- FPGA-Based_Counter_System.srcs/sources_1/new/up_down_counter.v
+++ FPGA-Based_Counter_System.srcs/sources_1/new/up_down_counter.v
@@ -1,26 +1,20 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.09.2026 14:53:42
-// Design Name: 
-// Module Name: up_down_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module up_down_counter #(parameter N = 4) (
+    input wire clk,       // Clock signal
+    input wire rst,       // Active-high synchronous reset
+    input wire en,        // Enable signal
+    input wire dir,       // Direction control: 1 for Up, 0 for Down
+    output reg [N-1:0] q  // N-bit output
+);
 
-
-module up_down_counter(
-
-    );
+    always @(posedge clk) begin
+        if (rst) begin
+            q <= 0;           
+        end else if (en) begin
+            if (dir) begin
+                q <= q + 1;   
+            end else begin
+                q <= q - 1;   
+            end
+        end
+    end
 endmodule
