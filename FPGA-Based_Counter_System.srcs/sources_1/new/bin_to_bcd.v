2dc70d5bba7d32c7e9af6f754ade445842390111
VanshAgarwal23 <vanshagrawalitarsi23@gmail.com>
2026-09-22 15:18:48 +0530

    designed 'bin_to_bcd' & 'display_mux' Modules

--- FPGA-Based_Counter_System.srcs/sources_1/new/bin_to_bcd.v
+++ FPGA-Based_Counter_System.srcs/sources_1/new/bin_to_bcd.v
@@ -1,26 +1,16 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.09.2026 15:00:25
-// Design Name: 
-// Module Name: bin_to_bcd
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bin_to_bcd(
-
-    );
+    input [3:0] bin,
+    output reg [3:0] tens,
+    output reg [3:0] ones
+);
+    // Simple logic since our max value is only 15 (4'b1111)
+    always @(*) begin
+        if (bin >= 10) begin
+            tens = 4'd1;
+            ones = bin - 4'd10;
+        end else begin
+            tens = 4'd0;
+            ones = bin;
+        end
+    end
 endmodule
--- FPGA-Based_Counter_System.srcs/sources_1/new/display_mux.v
+++ FPGA-Based_Counter_System.srcs/sources_1/new/display_mux.v
@@ -1,26 +1,31 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.09.2026 15:00:53
-// Design Name: 
-// Module Name: display_mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module display_mux #(parameter DIVISOR = 2) ( 
+    input clk,
+    input [3:0] bcd_tens,
+    input [3:0] bcd_ones,
+    output reg [3:0] current_digit,
+    output reg [3:0] an
+);
+    reg [19:0] counter = 0;
+    reg toggle = 0;
 
+    // Clock divider to control multiplexing speed
+    always @(posedge clk) begin
+        if (counter == DIVISOR - 1) begin
+            counter <= 0;
+            toggle <= ~toggle;
+        end else begin
+            counter <= counter + 1;
+        end
+    end
 
-module display_mux(
-
-    );
+    // Toggle between the Ones digit and Tens digit
+    always @(*) begin
+        if (toggle == 0) begin
+            an = 4'b1110;          // Turn on right-most digit (Ones)
+            current_digit = bcd_ones; // Send Ones data to decoder
+        end else begin
+            an = 4'b1101;          // Turn on second digit (Tens)
+            current_digit = bcd_tens; // Send Tens data to decoder
+        end
+    end
 endmodule
